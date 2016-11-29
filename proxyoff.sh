363a250930d3fe6e3268d834c027fadf9206df84
Kirill Vercetti <office@kyzima-spb.com>
2016-11-29 14:47:16 +0300

    Added "no_proxy" argument for proxy*.sh

--- proxyoff.sh
+++ proxyoff.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ $(id -u) -ne 0 ]; then
-  echo "This script must be run as root";
-  exit 1;
+    echo "This script must be run as root"
+    exit 1
 fi
 
-gsettings set org.gnome.system.proxy mode 'none' ;
+gsettings set org.gnome.system.proxy mode "none"
 
-grep PATH /etc/environment > lol.t;
-cat lol.t > /etc/environment;
+grep PATH /etc/environment > lol.t
+cat lol.t > /etc/environment
 
-printf "" > /etc/apt/apt.conf.d/95proxies;
+printf "" > /etc/apt/apt.conf.d/95proxies
 
-rm -rf lol.t;
+rm -rf lol.t
